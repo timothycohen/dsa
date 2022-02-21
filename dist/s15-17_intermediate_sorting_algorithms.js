098441509a4542b16bf683a82e898b8e9b75fa58
timothycohen <tco.physics@gmail.com>
2022-02-21 17:41:39 -0500

    finished intermediate sorting algorithms (marking the end of the algorithms section of the course)

--- dist/s15-17_intermediate_sorting_algorithms.js
+++ dist/s15-17_intermediate_sorting_algorithms.js
@@ -1,57 +1,55 @@
 "use strict";
-const mergeTwoSortedArrays = (arr1, arr2) => {
-    let smallestIndex1 = 0;
-    let smallestIndex2 = 0;
-    let mergedSection = new Array(arr1.length + arr2.length);
-    for (let i = 0; i < mergedSection.length; i++) {
-        if (arr1[smallestIndex1] < arr2[smallestIndex2] || arr2[smallestIndex2] === undefined) {
-            mergedSection[i] = arr1[smallestIndex1];
-            smallestIndex1++;
-        }
-        else {
-            mergedSection[i] = arr2[smallestIndex2];
-            smallestIndex2++;
-        }
-    }
-    return mergedSection;
-};
+// const mergeSort = (arr: number[]): number[] => {
+//   // split the array in two
+//   let arr2 = arr.splice(Math.ceil(arr.length / 2))
+//   // keep splitting recursively until they're at a single element
+//   if (arr2.length > 1) arr2 = mergeSort(arr2)
+//   if (arr.length > 1) arr = mergeSort(arr)
+//   // console.log(`MERGING ${arr} AND ${arr2}`)
+//   return mergeTwoSortedArrays(arr, arr2)
+// }
 const mergeSort = (arr) => {
-    // split the array in two
-    let arr2 = arr.splice(Math.ceil(arr.length / 2));
-    // keep splitting recursively until they're at a single element
-    if (arr2.length > 1)
-        arr2 = mergeSort(arr2);
-    if (arr.length > 1)
-        arr = mergeSort(arr);
-    // console.log(`MERGING ${arr} AND ${arr2}`)
-    return mergeTwoSortedArrays(arr, arr2);
-};
-const mergeSort2 = (arr) => {
+    const mergeTwoSortedArrays = (arr1, arr2) => {
+        let smallestIndex1 = 0;
+        let smallestIndex2 = 0;
+        let mergedSection = new Array(arr1.length + arr2.length);
+        for (let i = 0; i < mergedSection.length; i++) {
+            if (arr1[smallestIndex1] < arr2[smallestIndex2] || arr2[smallestIndex2] === undefined) {
+                mergedSection[i] = arr1[smallestIndex1];
+                smallestIndex1++;
+            }
+            else {
+                mergedSection[i] = arr2[smallestIndex2];
+                smallestIndex2++;
+            }
+        }
+        return mergedSection;
+    };
     // keep splitting recursively until they're at a single element
     if (arr.length === 1)
         return arr;
     const mid = Math.ceil(arr.length / 2);
-    const arr1 = mergeSort2(arr.slice(0, mid));
-    const arr2 = mergeSort2(arr.slice(mid));
+    const arr1 = mergeSort(arr.slice(0, mid));
+    const arr2 = mergeSort(arr.slice(mid));
     // console.log(`MERGING ${arr1} AND ${arr2}`)
     return mergeTwoSortedArrays(arr1, arr2);
 };
-// mutatesArray by placing everything less than the pivot to the left and greater than the right
-// returns the index of the pivot
-const pivotHelper = (arr, start, end) => {
-    const pivotIndex = start;
-    let lessThanCount = 0;
-    for (let i = start + 1; i <= end; i++) {
-        if (arr[i] < arr[pivotIndex]) {
-            lessThanCount++;
-            [arr[i], arr[pivotIndex + lessThanCount]] = [arr[pivotIndex + lessThanCount], arr[i]];
-        }
-    }
-    let newPivotIndex = pivotIndex + lessThanCount;
-    [arr[pivotIndex], arr[newPivotIndex]] = [arr[newPivotIndex], arr[pivotIndex]];
-    return newPivotIndex;
-};
 const quickSort = (arr, left = 0, right = arr.length - 1) => {
+    // mutatesArray by placing everything less than the pivot to the left and greater than the right
+    // returns the index of the pivot
+    const pivotHelper = (arr, start, end) => {
+        const pivotIndex = start;
+        let lessThanCount = 0;
+        for (let i = start + 1; i <= end; i++) {
+            if (arr[i] < arr[pivotIndex]) {
+                lessThanCount++;
+                [arr[i], arr[pivotIndex + lessThanCount]] = [arr[pivotIndex + lessThanCount], arr[i]];
+            }
+        }
+        let newPivotIndex = pivotIndex + lessThanCount;
+        [arr[pivotIndex], arr[newPivotIndex]] = [arr[newPivotIndex], arr[pivotIndex]];
+        return newPivotIndex;
+    };
     if (left >= right)
         return arr;
     const pivotIndex = pivotHelper(arr, left, right);
@@ -59,5 +57,50 @@ const quickSort = (arr, left = 0, right = arr.length - 1) => {
     quickSort(arr, pivotIndex + 1, right);
     return arr;
 };
-console.log("quickSort([1,4,99,32,43,2,43,6,9,7,11])", quickSort([1, 4, 99, 32, 43, 2, 43, 6, 9, 7, 11]));
-console.log("mergeSort([1,4,99,32,43,2,43,6,9,7,11])", mergeSort([1, 4, 99, 32, 43, 2, 43, 6, 9, 7, 11]));
+const radixSort = (arr, pos = 0) => {
+    const bucketize = (arr, pos) => {
+        const getNumAtPos = (num, pos) => {
+            let arr = num.toString().split('');
+            if (pos > (arr.length - 1))
+                return 0;
+            return parseInt(arr[arr.length - 1 - pos], 10);
+        };
+        return arr.reduce((acc, curr, index) => {
+            let bucketI = getNumAtPos(curr, pos);
+            acc[bucketI] = [...acc[bucketI], curr];
+            return acc;
+        }, new Array(10).fill([]));
+    };
+    let bucket = bucketize(arr, pos);
+    for (let i = 1; i < bucket.length - 1; i++) {
+        if (bucket[i].length)
+            break;
+        return bucket.flat();
+    }
+    return radixSort(bucket.flat(), ++pos);
+};
+const radixSort2 = (arr, pos = 0) => {
+    const getDigit = (num, pos) => Math.floor(Math.abs(num) / Math.pow(10, pos)) % 10;
+    const digitCount = (num) => {
+        if (num === 0)
+            return 1;
+        return Math.floor(Math.log10(Math.abs(num))) + 1;
+    };
+    const mostDigits = (arr) => arr.reduce((acc, curr) => Math.max(digitCount(curr), acc), 0);
+    const bucketize = (arr, pos) => {
+        let bucket = new Array(10).fill([]);
+        for (let i = 0; i < arr.length; i++) {
+            let bucketI = getDigit(arr[i], pos);
+            bucket[bucketI] = [...bucket[bucketI], arr[i]];
+        }
+        return bucket;
+    };
+    for (let i = 0; i < mostDigits(arr); i++) {
+        arr = bucketize(arr, i).flat();
+    }
+    return arr;
+};
+console.log("mergeSort([1,4,302,99,32,21,243,2,7443,36,9,7,11])", mergeSort([1, 4, 302, 99, 32, 21, 243, 2, 7443, 36, 9, 7, 11]));
+console.log("quickSort([1,4,302,99,32,21,243,2,7443,36,9,7,11])", quickSort([1, 4, 302, 99, 32, 21, 243, 2, 7443, 36, 9, 7, 11]));
+console.log("radixSort([1,4,302,99,32,21,243,2,7443,36,9,7,11])", radixSort([1, 4, 302, 99, 32, 21, 243, 2, 7443, 36, 9, 7, 11]));
+console.log("radixSort2([1,4,302,99,32,21,243,2,7443,36,9,7,11])", radixSort2([1, 4, 302, 99, 32, 21, 243, 2, 7443, 36, 9, 7, 11]));
--- src/s15-17_intermediate_sorting_algorithms.ts
+++ src/s15-17_intermediate_sorting_algorithms.ts
@@ -1,62 +1,62 @@
-const mergeTwoSortedArrays = (arr1: number[], arr2: number[]): number[] => {
-  let smallestIndex1 = 0;
-  let smallestIndex2 = 0;
-  let mergedSection = new Array<number>(arr1.length + arr2.length);
+// const mergeSort = (arr: number[]): number[] => {
+//   // split the array in two
+//   let arr2 = arr.splice(Math.ceil(arr.length / 2))
 
-  for (let i = 0; i < mergedSection.length; i++) {
-    if (arr1[smallestIndex1] < arr2[smallestIndex2] || arr2[smallestIndex2] === undefined) {
-      mergedSection[i] = arr1[smallestIndex1]
-      smallestIndex1++
-    } else {
-      mergedSection[i] = arr2[smallestIndex2]
-      smallestIndex2++
-    }
-  }
-  return mergedSection
-}
+//   // keep splitting recursively until they're at a single element
+//   if (arr2.length > 1) arr2 = mergeSort(arr2)
+//   if (arr.length > 1) arr = mergeSort(arr)
+
+//   // console.log(`MERGING ${arr} AND ${arr2}`)
+//   return mergeTwoSortedArrays(arr, arr2)
+// }
 
 const mergeSort = (arr: number[]): number[] => {
-  // split the array in two
-  let arr2 = arr.splice(Math.ceil(arr.length / 2))
+  const mergeTwoSortedArrays = (arr1: number[], arr2: number[]): number[] => {
+    let smallestIndex1 = 0;
+    let smallestIndex2 = 0;
+    let mergedSection = new Array<number>(arr1.length + arr2.length);
 
-  // keep splitting recursively until they're at a single element
-  if (arr2.length > 1) arr2 = mergeSort(arr2)
-  if (arr.length > 1) arr = mergeSort(arr)
+    for (let i = 0; i < mergedSection.length; i++) {
+      if (arr1[smallestIndex1] < arr2[smallestIndex2] || arr2[smallestIndex2] === undefined) {
+        mergedSection[i] = arr1[smallestIndex1]
+        smallestIndex1++
+      } else {
+        mergedSection[i] = arr2[smallestIndex2]
+        smallestIndex2++
+      }
+    }
+    return mergedSection
+  }
 
-  // console.log(`MERGING ${arr} AND ${arr2}`)
-  return mergeTwoSortedArrays(arr, arr2)
-}
-
-const mergeSort2 = (arr: number[]): number[] => {
   // keep splitting recursively until they're at a single element
   if (arr.length === 1) return arr
   const mid = Math.ceil(arr.length / 2)
-  const arr1 = mergeSort2(arr.slice(0, mid))
-  const arr2 = mergeSort2(arr.slice(mid))
+  const arr1 = mergeSort(arr.slice(0, mid))
+  const arr2 = mergeSort(arr.slice(mid))
 
   // console.log(`MERGING ${arr1} AND ${arr2}`)
   return mergeTwoSortedArrays(arr1, arr2)
 }
 
-// mutatesArray by placing everything less than the pivot to the left and greater than the right
-// returns the index of the pivot
-const pivotHelper = (arr: number[], start: number, end: number): number => {
-  const pivotIndex = start;
-  let lessThanCount = 0;
-
-  for (let i = start + 1; i <= end; i++) {
-    if (arr[i] < arr[pivotIndex]) {
-      lessThanCount++
-      [arr[i], arr[pivotIndex + lessThanCount]] = [arr[pivotIndex + lessThanCount], arr[i]]
-    }
-  }
-  let newPivotIndex = pivotIndex + lessThanCount;
-  [arr[pivotIndex], arr[newPivotIndex]] = [arr[newPivotIndex], arr[pivotIndex]];
-
-  return newPivotIndex
-}
-
 const quickSort = (arr: number[], left = 0, right = arr.length - 1): number[] => {
+  // mutatesArray by placing everything less than the pivot to the left and greater than the right
+  // returns the index of the pivot
+  const pivotHelper = (arr: number[], start: number, end: number): number => {
+    const pivotIndex = start;
+    let lessThanCount = 0;
+
+    for (let i = start + 1; i <= end; i++) {
+      if (arr[i] < arr[pivotIndex]) {
+        lessThanCount++
+        [arr[i], arr[pivotIndex + lessThanCount]] = [arr[pivotIndex + lessThanCount], arr[i]]
+      }
+    }
+    let newPivotIndex = pivotIndex + lessThanCount;
+    [arr[pivotIndex], arr[newPivotIndex]] = [arr[newPivotIndex], arr[pivotIndex]];
+
+    return newPivotIndex
+  }
+
   if (left >= right) return arr
   const pivotIndex = pivotHelper(arr, left, right)
   quickSort(arr, left, pivotIndex - 1)
@@ -65,5 +65,55 @@ const quickSort = (arr: number[], left = 0, right = arr.length - 1): number[] =>
   return arr
 }
 
-console.log("quickSort([1,4,99,32,43,2,43,6,9,7,11])", quickSort([1,4,99,32,43,2,43,6,9,7,11]))
-console.log("mergeSort([1,4,99,32,43,2,43,6,9,7,11])", mergeSort([1,4,99,32,43,2,43,6,9,7,11]))
+const radixSort = (arr: number[], pos: number = 0): number[] => {
+  const bucketize = (arr: number[], pos: number): number[][] => {
+    const getNumAtPos = (num: number, pos: number): number => {
+      let arr = num.toString().split('')
+      if (pos > (arr.length - 1)) return 0
+      return parseInt(arr[arr.length-1-pos], 10)
+    }
+
+    return arr.reduce((acc, curr, index) => {
+      let bucketI = getNumAtPos(curr, pos)
+      acc[bucketI] = [...acc[bucketI], curr]
+      return acc
+    }, new Array<number[]>(10).fill([]))
+  }
+
+  let bucket = bucketize(arr, pos)
+
+  for (let i = 1; i < bucket.length - 1; i++) {
+    if (bucket[i].length) break;
+    return bucket.flat()
+  }
+
+  return radixSort(bucket.flat(), ++pos)
+}
+
+const radixSort2 = (arr: number[], pos: number = 0): number[] => {
+  const getDigit = (num: number, pos: number) => Math.floor(Math.abs(num) / Math.pow(10, pos)) % 10;
+  const digitCount = (num: number): number => {
+    if (num === 0) return 1;
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
+  }
+  const mostDigits = (arr: number[]): number => arr.reduce((acc, curr) => Math.max(digitCount(curr), acc), 0)
+  const bucketize = (arr: number[], pos: number): number[][] => {
+    let bucket = new Array<number[]>(10).fill([])
+    for (let i = 0; i < arr.length; i++) {
+      let bucketI = getDigit(arr[i], pos)
+      bucket[bucketI] = [...bucket[bucketI], arr[i]]
+    }
+    return bucket;
+  }
+
+  for (let i = 0; i < mostDigits(arr); i++) {
+    arr = bucketize(arr, i).flat()
+  }
+
+  return arr;
+}
+
+console.log("mergeSort([1,4,302,99,32,21,243,2,7443,36,9,7,11])", mergeSort([1,4,302,99,32,21,243,2,7443,36,9,7,11]))
+console.log("quickSort([1,4,302,99,32,21,243,2,7443,36,9,7,11])", quickSort([1,4,302,99,32,21,243,2,7443,36,9,7,11]))
+console.log("radixSort([1,4,302,99,32,21,243,2,7443,36,9,7,11])", radixSort([1,4,302,99,32,21,243,2,7443,36,9,7,11]))
+console.log("radixSort2([1,4,302,99,32,21,243,2,7443,36,9,7,11])", radixSort2([1,4,302,99,32,21,243,2,7443,36,9,7,11]))
