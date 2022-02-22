9c2d7935c439119721884c612c4badad8d715263
timothycohen <tco.physics@gmail.com>
2022-02-22 10:26:38 -0500

    implemented a singly linked list and its tests

---
+++ src/s19_singly_linked_lists.ts
@@ -0,0 +1,160 @@
+// a singly linked list is similar to an array, but instead of indices it has nodes which point to each other with a next property
+// the list itself only has three properties: head node, tail node, and length.
+
+class SinglyLinkedListNode<T> {
+  val: T;
+  next: null | SinglyLinkedListNode<unknown>;
+  constructor(val: T) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  length: number;
+  head: SinglyLinkedListNode<unknown> | null;
+  tail: SinglyLinkedListNode<unknown> | null;
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push<T>(val: T): SinglyLinkedList {
+    // make new node
+    let n = new SinglyLinkedListNode(val);
+    // if it's the first node, make head and tail both the new node
+    if (this.head === null) {
+      this.head = n;
+      this.tail = n;
+    } else {
+    // otherwise point the tail to the new node and make the new node the tail
+      this.tail!.next = n;
+      this.tail = n;
+    }
+    // add 1 to length
+    this.length++;
+    return this;
+  }
+
+  pop(): SinglyLinkedListNode<unknown> | undefined {
+    let tail = this.tail
+    if (this.length === 0) return undefined
+    else if (this.length === 1) {
+      this.tail = null;
+      this.head = null;
+      this.length--;
+      return tail!;
+    }
+
+    let node = this.head;
+    for (let i = 0; i < this.length - 2; i++) {
+      node = node!.next
+    }
+    this.tail = node
+    this!.tail!.next = null
+    this.length--
+    return tail!
+  }
+
+  shift(): SinglyLinkedListNode<unknown> | undefined {
+    if (this.head === null) return undefined
+    let head = this.head;
+    this.head = this.head.next
+    head.next = null;
+    this.length--;
+    if (this.length === 0) this.tail = null;
+    return head;
+  }
+
+  unshift<T>(val: T): SinglyLinkedList {
+    let node = new SinglyLinkedListNode(val)
+    node.next = this.head;
+    this.head = node;
+    this.length++;
+    if (this.length === 1) {
+      this.tail = node;
+    }
+    return this
+  }
+
+  get(index: number): SinglyLinkedListNode<unknown> | undefined {
+    if (index < 0 || index >= this.length) return undefined;
+
+    let node = this.head
+    for (let i = 0; i < index; i++) {
+      node = node!.next;
+    }
+    return node as SinglyLinkedListNode<unknown>;
+  }
+
+  set<T>(index: number, val: T): boolean {
+    let node = this.get(index)
+    if (node === undefined) return false
+    node.val = val;
+    return true;
+  }
+
+  insert<T>(index: number, val: T): boolean {
+    if (index === 0) return !!this.unshift(val)
+    if (index === this.length) return !!this.push(val)
+    if (index > this.length || index < 0) return false
+
+    let newNode = new SinglyLinkedListNode(val);
+    let oldNode = this.get(index - 1)
+    newNode.next = oldNode!.next;
+    oldNode!.next = newNode
+    this.length++
+    return true;
+  }
+
+  remove(index: number): SinglyLinkedListNode<unknown> | undefined {
+    if (index === 0) return this.shift()
+    if (index === this.length - 1) return this.pop()
+    if (index > this.length - 1 || index < 0) return undefined
+
+    const prevNode = this.get(index - 1)
+    const removedNode = prevNode!.next
+    prevNode!.next = removedNode!.next
+    this.length--;
+
+    return removedNode!
+  }
+
+  reverse(): SinglyLinkedList {
+    // if the list is of length 0 or 1, a reverse will have no effect
+    if (this.length < 2) return this
+
+    // move through the list with both the current and next nodes
+    let currentNode = this.head!
+    let oldNextNewPrev = currentNode.next!;
+
+    // loop until one before the tail
+    for (let i = 0; i < this.length - 2; i++) {
+      // store the next node in a temp variable so the node can be progressed forward even after the next property has been swapped
+      let tempNext = oldNextNewPrev.next!
+      // flip the pointer so now the next is pointing to the current
+      oldNextNewPrev.next = currentNode
+      // move the current and next nodes forward one
+      currentNode = oldNextNewPrev
+      oldNextNewPrev = tempNext
+    }
+    // handle the head and tail separately
+    // flip the pointer
+    oldNextNewPrev.next = currentNode
+    // swap the head and tail
+    let oldHead = this.head;
+    this.head = oldNextNewPrev
+    this.tail = oldHead
+    // set the tail.next to null
+    this.tail!.next = null;
+
+    return this;
+  }
+
+  print(): void {
+    console.log(JSON.stringify(this, null, 2))
+  }
+}
+
+module.exports = SinglyLinkedList
---
+++ tests/s19_singly_linked_lists.test.js
@@ -0,0 +1,166 @@
+const SinglyLinkedList = require('../dist/s19_singly_linked_lists')
+
+test('new list', () => {
+  let list = new SinglyLinkedList;
+  expect(list.head).toBe(null);
+  expect(list.tail).toBe(null);
+  expect(list.length).toBe(0);
+});
+test('.push adds one to the end and returns the list', () => {
+  let list = new SinglyLinkedList;
+  list.push(5)
+  expect(list.head.val).toBe(5);
+  expect(list.head.next).toBe(null);
+  expect(list.tail.val).toBe(5);
+  expect(list.tail.next).toBe(null);
+  expect(list.length).toBe(1);
+  list.push(7)
+  expect(list.head.val).toBe(5);
+  expect(list.head.next).toBe(list.tail);
+  expect(list.tail.val).toBe(7);
+  expect(list.tail.next).toBe(null);
+  expect(list.length).toBe(2);
+})
+test('.pop removes one from the end and returns the node or undefined', () => {
+  let list = new SinglyLinkedList;
+  expect(list.pop()).toBe(undefined);
+
+  list.push(5)
+  let popped5 = list.pop();
+  expect(popped5.val).toBe(5);
+  expect(list.head).toBe(null);
+  expect(list.tail).toBe(null);
+  expect(list.length).toBe(0);
+
+
+  list.push(1)
+  list.push(3)
+  list.push(7)
+  let popped7 = list.pop();
+
+  expect(popped7.val).toBe(7);
+  expect(list.length).toBe(2);
+  expect(list.head.val).toBe(1);
+  expect(list.tail.val).toBe(3);
+  expect(list.head.next).toBe(list.tail);
+})
+test('.shift removes one from the start and returns the node or undefined', () => {
+  let list = new SinglyLinkedList;
+  expect(list.shift()).toBe(undefined);
+
+  list.push(5)
+  let shifted5 = list.shift();
+  expect(shifted5.val).toBe(5);
+  expect(list.head).toBe(null);
+  expect(list.tail).toBe(null);
+  expect(list.length).toBe(0);
+
+  list.push(1)
+  list.push(3)
+  list.push(7)
+  let shifted1 = list.shift();
+
+  expect(shifted1.val).toBe(1);
+  expect(list.length).toBe(2);
+  expect(list.head.val).toBe(3);
+  expect(list.tail.val).toBe(7);
+  expect(list.head.next).toBe(list.tail);
+})
+test('.unshift adds one to the start and returns the list', () => {
+  let list = new SinglyLinkedList;
+  list.unshift(5)
+  expect(list.head.val).toBe(5);
+  expect(list.head.next).toBe(null);
+  expect(list.tail.val).toBe(5);
+  expect(list.tail.next).toBe(null);
+  expect(list.length).toBe(1);
+  list.unshift(7)
+  expect(list.head.val).toBe(7);
+  expect(list.head.next).toBe(list.tail);
+  expect(list.tail.val).toBe(5);
+  expect(list.tail.next).toBe(null);
+  expect(list.length).toBe(2);
+})
+test('.get returns the node at a position or undefined', () => {
+  let list = new SinglyLinkedList;
+  expect(list.get(0)).toBe(undefined)
+  list.push(5)
+  list.push(7)
+  list.push(3)
+  expect(list.get(0).val).toBe(5)
+  expect(list.get(1).val).toBe(7)
+  expect(list.get(2).val).toBe(3)
+  expect(list.get(-1)).toBe(undefined)
+  expect(list.get(550)).toBe(undefined)
+})
+test('.set changes the value of a node at a certain position and returns true/false based on success', () => {
+  let list = new SinglyLinkedList;
+  expect(list.set(0, 'hi')).toBe(false)
+
+  list.push('hello')
+  expect(list.set(-1, 'hi')).toBe(false)
+  expect(list.set(0, 'hi')).toBe(true)
+  expect(list.set(1, 'hi')).toBe(false)
+  expect(list.get(0).val).toBe('hi')
+
+  list.push('there')
+  expect(list.set(1, '!')).toBe(true)
+  expect(list.get(0).val).toBe('hi')
+  expect(list.get(1).val).toBe('!')
+})
+test('.insert adds a node at the position and returns true/false based on success', () => {
+  let list = new SinglyLinkedList;
+  expect(list.insert(-1, 'hi')).toBe(false)
+  expect(list.insert(1, 'hi')).toBe(false)
+  expect(list.insert(0, 'hi')).toBe(true)
+  expect(list.get(0).val).toBe('hi')
+  list.push(1)
+  list.push(2)
+  list.push(3)
+  expect(list.insert(3, '!!!')).toBe(true)
+  expect(list.get(3).val).toBe('!!!')
+  expect(list.tail.val).toBe(3)
+  expect(list.insert(1, 'wow')).toBe(true)
+  expect(list.head.next.val).toBe('wow')
+})
+test('.remove deletes a node at the position and returns the removed node or undefined', () => {
+  let list = new SinglyLinkedList;
+  expect(list.remove(0)).toBe(undefined)
+  expect(list.remove(-1)).toBe(undefined)
+  expect(list.remove(1)).toBe(undefined)
+  list.push(0)
+  list.push('1')
+  list.push(2)
+  list.push(3)
+  expect(list.remove(1).val).toBe('1')
+  expect(list.get(1).val).toBe(2)
+})
+test('.reverse reverses the list', () => {
+  let list = new SinglyLinkedList;
+  list.push(1)
+  list.push(2)
+  list.push(3)
+  list.push(4)
+  list.push(5)
+  list.reverse()
+  expect(list.head.val).toBe(5)
+  expect(list.tail.val).toBe(1)
+  expect(list.get(1).val).toBe(4)
+})
+test.skip('.print pretty prints the list', () => {
+  let list = new SinglyLinkedList;
+  list.push(1)
+  list.push(2)
+  list.push(3)
+  list.push(4)
+  list.push(5)
+  list.push(6)
+  list.push(7)
+  list.push(8)
+  list.push(9)
+  list.push(10)
+
+  const consoleSpy = jest.spyOn(console, 'log');
+  list.print()
+  expect(consoleSpy).toHaveBeenCalledWith(JSON.stringify(list, null, 2));
+})
